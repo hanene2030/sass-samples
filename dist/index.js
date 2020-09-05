823183e5ce2bf79bb6c9dcf50f9be1d73bd85fd1
unknown <hanene2030@gmail.com>
2020-09-05 10:12:44 +0200

    First commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+const logo=document.querySelectorAll('#logo path');
+console.log(logo);
+
+
+for(let i = 0 ; i<logo.length; i++){
+   // console.log(logo[i]);
+    console.log('Letter '+i+ 'is'+ logo[i].getTotalLength()   );
+
+}
